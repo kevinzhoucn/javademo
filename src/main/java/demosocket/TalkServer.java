4b7b020aec19fc072f572e6923f1a08dde35d673
zzpf <zzpf@163.com>
2016-08-09 18:02:51 +0800

    demo socket

---
+++ src/main/java/demosocket/TalkServer.java
@@ -0,0 +1,47 @@
+package demosocket;
+
+import java.io.*;
+import java.net.*;
+
+public class TalkServer {
+    public static void main(String args[]) {
+        try {
+            ServerSocket server = null;
+            try {
+                server = new ServerSocket(4700);
+            } catch (Exception e) {
+                System.out.println("Can not listen to: " + e);
+            }
+
+            Socket socket = null;
+            try {
+                socket = server.accept();                
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
+
+            String line;
+            BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter os = new PrintWriter(socket.getOutputStream());
+            BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Client: " + is.readLine());
+            line = sin.readLine();
+
+            while( !line.equals("bye") ) {
+            // while (true) {
+                os.println(line);
+                os.flush();
+                System.out.println("Server: " + line);
+                System.out.println("Client: " + is.readLine());
+                line = sin.readLine();
+            }
+
+            os.close();
+            is.close();
+            socket.close();
+            server.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
